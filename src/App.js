1e180287d976554929cfb0e11982d880b5e3ffb8
최정훈 <jeongssi94@gmail.com>
2022-12-05 23:09:43 +0900

    Add:정렬기능 프리티어추가

--- src/App.js
+++ src/App.js
@@ -4,53 +4,76 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  // let [글제목1, a] = useState("골든 구스 추천");
-  // let [글제목2, b] = useState("에르메스 가방 추천");
-  // let [글제목3, c] = useState("샤넬 가방 추천");
+  // let [Title1, a] = useState("골든 구스 추천");
+  // let [Title2, b] = useState("에르메스 가방 추천");
+  // let [Title3, c] = useState("샤넬 가방 추천");
   /*
   State는 변동시 자동으로 html에 반영되게 만들고 싶으면 사용(자주변경될꺼같은 것들만 작성한다)
   사이트의 로고 같은 경우에는 변경될일 없기때문에 state로 할 필요는 없다
   */
-  let [글제목,d] = useState([
+  let [Title, setTitle] = useState([
     "프라다 가방 추천",
     "TIME 가방 추천",
     "나이키 신발 추천",
   ]);
   //Array로 state 값을 뽑기
-  let[좋아요,e]=useState(0)
+  let [좋아요, e] = useState(0);
 
   return (
     <div className="App">
       <div className="Black-nav">
         <h4 style={{ color: "red", fontSize: "20px" }}>Brand Blog</h4>
       </div>
-      <button onClick={()=>{
-        let copy = ([...글제목]);
-        copy[0] ='샤넬가방 추천';
-        d(copy)
-        }}>글수정</button>
+      <button
+        onClick={() => {
+          let copy = [...Title];
+          copy.sort();
+          setTitle(copy);
+        }}
+      >
+        정렬
+      </button>
+      <button
+        onClick={() => {
+          let copy = [...Title];
+          copy[0] = "샤넬가방 추천";
+          setTitle(copy);
+        }}
+      >
+        글수정
+      </button>
       {/* <div className="list">
-        <h4>{글제목1}</h4>
+        <h4>{Title1}</h4>
         <p>11월 30일</p>
       </div>
       <div className="list">
-        <h4>{글제목2}</h4>
+        <h4>{Title2}</h4>
         <p>11월 30일</p>
       </div>
       <div className="list">
-        <h4>{글제목3}</h4>
+        <h4>{Title3}</h4>
         <p>11월 30일</p>
       </div> */}
       <div className="list">
-        <h4>{글제목[0]}<span onClick={()=>{e(좋아요+1)}}>👍</span>{좋아요}</h4>
+        <h4>
+          {Title[0]}
+          <span
+            onClick={() => {
+              e(좋아요 + 1);
+            }}
+          >
+            👍
+          </span>
+          {좋아요}
+        </h4>
         <p>11월 30일</p>
       </div>
       <div className="list">
-        <h4>{글제목[1]}</h4>
+        <h4>{Title[1]}</h4>
         <p>11월 30일</p>
       </div>
       <div className="list">
-        <h4>{글제목[2]}</h4>
+        <h4>{Title[2]}</h4>
         <p>11월 30일</p>
       </div>
     </div>
