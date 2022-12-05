739dd329f6866e3976b248f14a64e416351a057a
J.hoon <68211978+Choi-jeonghoon@users.noreply.github.com>
2022-12-05 15:41:49 +0900

    Add: onClick 기능 연습

--- src/App.js
+++ src/App.js
@@ -11,19 +11,21 @@ function App() {
   State는 변동시 자동으로 html에 반영되게 만들고 싶으면 사용(자주변경될꺼같은 것들만 작성한다)
   사이트의 로고 같은 경우에는 변경될일 없기때문에 state로 할 필요는 없다
   */
-  let [글제목] = useState([
+  let [글제목,d] = useState([
     "프라다 가방 추천",
     "TIME 가방 추천",
     "나이키 신발 추천",
   ]);
   //Array로 state 값을 뽑기
+  let[좋아요,e]=useState(0)
+
   return (
     <div className="App">
       <div className="Black-nav">
-        <h4 style={{ color: "red", fontSize: "20px" }}>브랜드 블로그</h4>
+        <h4 style={{ color: "red", fontSize: "20px" }}>Brand Blog</h4>
       </div>
       <div className="list">
-        <h4>{글제목1}</h4>
+        <h4>{글제목1}<span onClick={()=>{e(좋아요+1)}}>👍</span>{좋아요}</h4>
         <p>11월 30일</p>
       </div>
       <div className="list">
