bc421b964cc519e4a1baccfcf4dd49302c8ce393
J.hoon <68211978+Choi-jeonghoon@users.noreply.github.com>
2022-12-05 16:31:06 +0900

    Add:글수정 버튼을 이용해 첫번쨰 글 수정기능

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let [글제목1, a] = useState("골든 구스 추천");
-  let [글제목2, b] = useState("에르메스 가방 추천");
-  let [글제목3, c] = useState("샤넬 가방 추천");
+  // let [글제목1, a] = useState("골든 구스 추천");
+  // let [글제목2, b] = useState("에르메스 가방 추천");
+  // let [글제목3, c] = useState("샤넬 가방 추천");
   /*
   State는 변동시 자동으로 html에 반영되게 만들고 싶으면 사용(자주변경될꺼같은 것들만 작성한다)
   사이트의 로고 같은 경우에는 변경될일 없기때문에 state로 할 필요는 없다
@@ -24,8 +24,13 @@ function App() {
       <div className="Black-nav">
         <h4 style={{ color: "red", fontSize: "20px" }}>Brand Blog</h4>
       </div>
-      <div className="list">
-        <h4>{글제목1}<span onClick={()=>{e(좋아요+1)}}>👍</span>{좋아요}</h4>
+      <button onClick={()=>{
+        let copy = ([...글제목]);
+        copy[0] ='샤넬가방 추천';
+        d(copy)
+        }}>글수정</button>
+      {/* <div className="list">
+        <h4>{글제목1}</h4>
         <p>11월 30일</p>
       </div>
       <div className="list">
@@ -35,9 +40,9 @@ function App() {
       <div className="list">
         <h4>{글제목3}</h4>
         <p>11월 30일</p>
-      </div>
+      </div> */}
       <div className="list">
-        <h4>{글제목[0]}</h4>
+        <h4>{글제목[0]}<span onClick={()=>{e(좋아요+1)}}>👍</span>{좋아요}</h4>
         <p>11월 30일</p>
       </div>
       <div className="list">
