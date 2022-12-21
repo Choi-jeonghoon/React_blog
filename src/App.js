d0c52ca0ba030ffb1b693b092fa73be70f315e36
J.hoon <68211978+Choi-jeonghoon@users.noreply.github.com>
2022-12-21 16:33:56 +0900

    Add:map사용

--- src/App.js
+++ src/App.js
@@ -12,13 +12,14 @@ function App() {
   State는 변동시 자동으로 html에 반영되게 만들고 싶으면 사용(자주변경될꺼같은 것들만 작성한다)
   사이트의 로고 같은 경우에는 변경될일 없기때문에 state로 할 필요는 없다
   */
-  const [Title, setTitle] = useState([
+  const [title, setTitle] = useState([
     "프라다 가방 추천",
     "TIME 가방 추천",
     "나이키 신발 추천",
   ]);
   //Array로 state 값을 뽑기
   const [좋아요, e] = useState(0);
+  const [like, setLike] = useState([0, 0, 0]);
   const [detail, setDetail] = useState(false);
 
   return (
@@ -28,7 +29,7 @@ function App() {
       </div>
       <button
         onClick={() => {
-          let copy = [...Title];
+          let copy = [...title];
           copy.sort();
           setTitle(copy);
         }}
@@ -37,7 +38,7 @@ function App() {
       </button>
       <button
         onClick={() => {
-          let copy = [...Title];
+          let copy = [...title];
           copy[0] = "샤넬가방 추천";
           setTitle(copy);
         }}
@@ -45,20 +46,21 @@ function App() {
         글수정
       </button>
       {/* <div className="list">
-        <h4>{Title1}</h4>
+        <h4>{title1}</h4>
         <p>11월 30일</p>
       </div>
       <div className="list">
-        <h4>{Title2}</h4>
+        <h4>{title2}</h4>
         <p>11월 30일</p>
       </div>
       <div className="list">
-        <h4>{Title3}</h4>
+        <h4>{title3}</h4>
         <p>11월 30일</p>
       </div> */}
-      <div className="list">
+
+      {/* <div className="list">
         <h4>
-          {Title[0]}
+          {title[0]}
           <span
             onClick={() => {
               e(좋아요 + 1);
@@ -71,26 +73,49 @@ function App() {
         <p>11월 30일</p>
       </div>
       <div className="list" style={{ cursor: "pointer" }}>
-        <h4>
-          {Title[1]}
-          {/* <span
-            onClick={() => {
-              e(좋아요 + 1);
-            }}
-          >
-            👍
-          </span> */}
-          {/* {좋아요} */}
-        </h4>
+        <h4>{title[1]}</h4>
         <p>11월 29일</p>
       </div>
       <div className="list" style={{ cursor: "pointer" }}>
         <h4 style={{ cursor: "pointer" }} onClick={() => setDetail(!detail)}>
-          {Title[2]}
+          {title[2]}
         </h4>
         <p>11월 30일</p>
-      </div>
+      </div> */}
+
+      {title.map(function (titleName, i) {
+        return (
+          <div className="list" key={i}>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <h4
+                style={{ cursor: "pointer" }}
+                onClick={() => setDetail(!detail)}
+              >
+                {titleName}
+              </h4>
+              <span
+                onClick={() => {
+                  let copy = [...like];
+                  copy[i]++;
+                  setLike(copy);
+                }}
+              >
+                👍
+                {like[i]}
+              </span>
+            </div>
+            {/* <h4>{title[i]} </h4> */}
+            {/* 
+            function에 파라미터는 2개까지 작명이 가능함 반복문이 돌떄마다 0부터 1씩 증가하는 정수이다. 위와같은 방식으로 작성이 가능하다
+            */}
+            <p>2월 10일</p>
+          </div>
+        );
+      })}
+
       {detail == true ? <Detail /> : null}
+      {/* if문은 리엑트상에서 작동하지않기때문에 삼향연산자로 작성해서 쓴다 위와 같은모습으로
+      위의 있는 코드는 detail state의 값이 참이면 Detail컴포넌트를 보여주고 그게아니면 보여주지 않는다는 의미의 code */}
     </div>
   );
 }
